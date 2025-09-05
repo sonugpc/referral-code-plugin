9f53515aea1da153b97fc50c3364112e4ef55981
Sonu Gupta <sonugpc@gmail.com>
2025-09-05 21:52:10 +0530

    Fixed editors

--- js/editor.js
+++ js/editor.js
@@ -1,8 +1,75 @@
 ( function ( wp ) {
+    const { registerPlugin } = wp.plugins;
+    const { PluginDocumentSettingPanel } = wp.editPost;
     const { render, createElement, useState } = wp.element;
     const { TextControl, TextareaControl, Button, PanelBody, ToggleControl } = wp.components;
     const { useSelect, useDispatch } = wp.data;
     const { __ } = wp.i18n;
+
+    const ReferralCodeMetaFields = () => {
+        const postType = useSelect( ( select ) => select( 'core/editor' ).getCurrentPostType() );
+        if ( postType !== 'referral-codes' ) {
+            return null;
+        }
+
+        const meta = useSelect( ( select ) => select( 'core/editor' ).getEditedPostAttribute( 'meta' ) || {} );
+        const { referral_code, referral_link, signup_bonus, referral_rewards, app_name } = meta;
+
+        const { editPost } = useDispatch( 'core/editor' );
+
+        const setMetaValue = ( key, value ) => {
+            editPost( { meta: { ...meta, [key]: value } } );
+        };
+
+        return createElement(
+            PluginDocumentSettingPanel,
+            {
+                name: 'referral-code-meta-fields',
+                title: __( 'Referral Code Details', 'referral-code-plugin' ),
+                className: 'referral-code-meta-fields',
+            },
+            createElement(
+                TextControl,
+                {
+                    label: __( 'Referral Code', 'referral-code-plugin' ),
+                    value: referral_code,
+                    onChange: ( value ) => setMetaValue( 'referral_code', value ),
+                }
+            ),
+            createElement(
+                TextControl,
+                {
+                    label: __( 'Referral Link', 'referral-code-plugin' ),
+                    value: referral_link,
+                    onChange: ( value ) => setMetaValue( 'referral_link', value ),
+                }
+            ),
+            createElement(
+                TextControl,
+                {
+                    label: __( 'Sign-up Bonus', 'referral-code-plugin' ),
+                    value: signup_bonus,
+                    onChange: ( value ) => setMetaValue( 'signup_bonus', value ),
+                }
+            ),
+            createElement(
+                TextControl,
+                {
+                    label: __( 'Referral Rewards', 'referral-code-plugin' ),
+                    value: referral_rewards,
+                    onChange: ( value ) => setMetaValue( 'referral_rewards', value ),
+                }
+            ),
+            createElement(
+                TextControl,
+                {
+                    label: __( 'App Name', 'referral-code-plugin' ),
+                    value: app_name,
+                    onChange: ( value ) => setMetaValue( 'app_name', value ),
+                }
+            )
+        );
+    };
 
     const FaqEditor = () => {
         const meta = useSelect( ( select ) => select( 'core/editor' ).getEditedPostAttribute( 'meta' ) || {} );
@@ -85,6 +152,11 @@
                 )
         );
     };
+
+    registerPlugin( 'referral-code-meta-fields', {
+        render: ReferralCodeMetaFields,
+        icon: 'money',
+    } );
 
     document.addEventListener('DOMContentLoaded', () => {
         const editorEl = document.getElementById('rcp-faq-editor');
